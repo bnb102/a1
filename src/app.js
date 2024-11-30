dbdf54e7ea2bf4cca1f03a6884fb4d73a5a113e0
bnb102 <bnb.binance.vn@gmail.com>
2024-12-01 00:58:53 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const listapp = [
+    {
+        name: 'JEFF',
+        interRate: 'Lãi suất 0% khoản vay đầu tiên',
+        limit: '5 - 30 triệu',
+        identificationDocuments: 'CMND/CCCD là có thể vay',
+        age: '20 - 60, nhận nợ xấu',
+        descreption: 'Duyệt sau 5 phút, tỉ lệ duyệt 99,98%!',
+        linkRef: 'https://ladipage.dinos.click/click?a=51338&o=161',
+        img: './src/jeff.png',
+    },
+    {
+        name: 'VAY VND',
+        interRate: 'Lãi suất 0% khoản vay đầu tiên',
+        limit: '5 - 15 triệu',
+        identificationDocuments: 'CMND/CCCD là có thể vay',
+        age: '22 tuổi trở lên, nhận nợ xấu.',
+        descreption: 'Duyệt sau 5 phút, tỉ lệ duyệt 99,99%!',
+        linkRef: 'https://dinos.scaletrk.com/click?a=51338&o=277',
+        img: './src/vayvnd.png',
+    },
+    {
+        name: '',
+        interRate: '',
+        limit: '',
+        identificationDocuments: '',
+        age: '',
+        descreption: '',
+        linkRef: '',
+        img: '',
+    },
+    {
+        name: '',
+        interRate: '',
+        limit: '',
+        identificationDocuments: '',
+        age: '',
+        descreption: '',
+        linkRef: '',
+        img: '',
+    },
+    {
+        name: '',
+        interRate: '',
+        limit: '',
+        identificationDocuments: '',
+        age: '',
+        descreption: '',
+        linkRef: '',
+        img: '',
+    }
+]
+
+console.log(listapp);
+console.log('hello world');
